0e1fa053797544945b15ea288c4f97d2d3eff589
Mohammed Obaid Raza <159754772+mohammedobaidraza@users.noreply.github.com>
2026-02-14 00:23:31 -0600

    Switch to HashRouter for routing
    Replaced BrowserRouter with HashRouter for routing.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 import Index from "./pages/Index";
@@ -33,7 +33,8 @@ const App = () => (
       <Toaster />
       <Sonner />
 
-      <BrowserRouter>
+      {/* ✅ Switched to HashRouter */}
+      <HashRouter>
         <ScrollToTop />
         <BackToTop />
 
@@ -41,18 +42,13 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/physicians" element={<Physicians />} />
-          {/* THIS WAS MISSING - ADDED HERE: */}
           <Route path="/patients" element={<Patients />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/contact" element={<Contact />} />
-
-          {/* Matches Footer link */}
           <Route path="/privacy" element={<PrivacyPolicy />} />
-
-          {/* Catch-all */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
